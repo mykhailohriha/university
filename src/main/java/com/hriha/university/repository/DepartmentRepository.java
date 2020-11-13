25a946ab70ae3bda635e2b7e937075b6ccfc728c
Mykhailo Hriha <mykhailho.hriha@gmail.com>
2020-11-13 11:16:51 +0200

    hotfix

--- src/main/java/com/hriha/university/repository/DepartmentRepository.java
+++ src/main/java/com/hriha/university/repository/DepartmentRepository.java
@@ -2,9 +2,12 @@ package com.hriha.university.repository;
 
 import com.hriha.university.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    @Query(value = "select d from Department d where d.name = :n")
     Department findByName(String name);
 }
